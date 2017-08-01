2a10e0ea8a20c44a1640ee00a6ccafff927dc381
babylab <babylab@sr15-4f84dfb0d1.stanford.edu>
2017-08-01 12:38:48 -0700

    removed redundancies

--- westheimer.m
+++ westheimer.m
@@ -1,171 +1,102 @@
+%%  NOTE
+%   This version of the Westheimer analysis script is compatible with the
+%   following paradigms: whmSuperSet, whmHexCancellation, &
+%   whmCancellation2Sz. whmMixed and whmHexFreq have been moved to a
+%   separate script.
+
 %% ADD CODE BASES TO PATH
 addpath(genpath('~/code/git/rcaBase'));
 addpath(genpath('~/code/git/mrC'));
 addpath(genpath('~/code/git/sweepAnalysis'));
+addpath(genpath('~/code/git/svndl'));
 
 %% SET UP: DEFINE VARIABLES & LOAD DATA
 clear all
 close all
 
-paradigm = 'pilot5'; % 'whmMixed' | 'whmHexFreq' | 'whmSuperSet' | 'whmHexCancellation'
-stimFrq = 3; % 3 for whmMixed, whmHexCancellation | [3.75,30/11,3] for whmHexFreq | [30/11,3,3.75] for whmSuperSet
-folder = '/Users/babylab/Desktop/whm';
-RCAfolder = sprintf('%s/RCA_pilot5',folder);
-[dataFolder,dataSet,names] = getInfo(folder,paradigm);
+paradigm = 'pilot5'; % 'whmSuperSet' | 'whmHexCancellation' | 'whmCancellation2Sz'
+stimFrq = [3]; % 3 for whmHexCancellation, whmCancellation2Sz | [30/11,3,3.75] for whmSuperSet
+nPol = 2;
+parentDir = '/Users/babylab/Desktop/whm';
+[dataFolder,dataSet,names,RCAfolder] = getInfo(parentDir,paradigm);
 
 tic
 for s = 1:length(dataSet)
     fprintf('Running subject %s\n', names{s});
     curDataFolder = sprintf('%s/%s', dataFolder,dataSet{s});   
-%     if length(stimFrq) == 1
-%         [fullData(:,:,:,:,s), fnlData(:,:,:,s), nCnd] = loadData(curDataFolder,stimFrq);   
-%     else
-        [fullData{s},fnlData{s}, nCnd] = loadData(curDataFolder,stimFrq); % orig rearrByCnd: chan x timept x cnd x tri
-%     end
+    [data{s},avgedData{s}, nCnd] = loadData(curDataFolder,stimFrq); % data: nChan x nTimept x nTri x nCnd
 end
 toc
+
 %% SORTING DATA FOR RCA
-% (samples x channels x trials), or a cell array (conditions x subjects)
-if length(stimFrq) == 1
-%     rearrData = permute(fullData,[2 1 4 3 5]);
-%     for a = 1:length(dataSet)
-%         for c = 1:nCnd
-%             RCA{c,a} = squeeze(rearrData(:,:,:,c,a));
-%         end
-%     end
-%     if strcmp(paradigm,'whmHexCancellation')
-%         allUR = RCA([1 5 9 13],:);
-%         allUL = RCA([1 5 9 13]+1,:);
-%         allLL = RCA([1 5 9 13]+2,:);
-%         allLR = RCA([1 5 9 13]+3,:);
-%         allF = RCA([1:4],:);
-%         allP1 = RCA([1:4]+1,:);
-%         allP2 = RCA([1:4]+2,:);
-%     end
-    
-    for a = 1:length(dataSet)
-        for c = 1:nCnd
-            RCA{c,a} = squeeze(fullData{a}(:,:,:,c));
-        end
-    end
-    
-elseif strcmp(paradigm,'whmHexFreq')
-    for s = 1:length(dataSet)
-        freq1(:,:,:,:,s) = permute(fullData{s}{1},[2 1 3 4]); % timecourse x chan x tri x cnd x subj, extracted from cell
-        freq2(:,:,:,:,s) = permute(fullData{s}{2},[2 1 3 4]);
-        freq3(:,:,:,:,s) = permute(fullData{s}{3},[2 1 3 4]);       
-    end
-    for a = 1:length(dataSet)
-        for c = 1:size(freq1,4)
-            RCA_frq1{c,a} = squeeze(freq1(:,:,:,c,a)); % cell array with all 6 cnd of one freq
-        end
-        for c = 1:size(freq2,4)
-            RCA_frq2{c,a} = squeeze(freq2(:,:,:,c,a));
-        end
-        for c = 1:size(freq3,4)
-            RCA_frq3{c,a} = squeeze(freq3(:,:,:,c,a));
-        end
-    end
-elseif strcmp(paradigm,'whmSuperSet')
-    % EFFECT OF FREQ: POOLING OVER ECC + POL
-    for s = 1:length(dataSet)
-        for c = 1:length(fullData{s})
-            fullData{s}{c} = permute(fullData{s}{c},[2 1 4 3]); % timept x chan x cnd x tri
-        end
-        reshapeByFrq{s} = reshape(fullData{s},length(stimFrq),[])';
-    end
-    for s = 1:length(dataSet)
-        for c = 1:size(reshapeByFrq{s},1)
-            cIdx = c*2-1;
-            for p = 1:2                
-                RCA_frq1{cIdx,s} = squeeze(reshapeByFrq{s}{c,1}(:,:,p,:)); 
-                RCA_frq2{cIdx,s} = squeeze(reshapeByFrq{s}{c,2}(:,:,p,:));
-                RCA_frq3{cIdx,s} = squeeze(reshapeByFrq{s}{c,3}(:,:,p,:));
-                cIdx = cIdx + 1;
-            end
-        end
-    end
-    % EFFECT OF ECCENTRICITY: POOLING OVER TF + POL
-    for s = 1:length(dataSet)
-        reshapeByEcc{s} = reshape(fullData{s},[],6);
-    end
-    for s = 1:length(dataSet)
-        for c = 1:size(reshapeByEcc{s},1)
-            cIdx = c*2-1;
-            for p = 1:2
-                RCA_SF{cIdx,s} = squeeze(reshapeByEcc{s}{c,1}(:,:,p,:)); % broken here
-                RCA_SP1{cIdx,s} = squeeze(reshapeByEcc{s}{c,2}(:,:,p,:));
-                RCA_SP2{cIdx,s} = squeeze(reshapeByEcc{s}{c,3}(:,:,p,:));
-                RCA_IF{cIdx,s} = squeeze(reshapeByEcc{s}{c,4}(:,:,p,:));
-                RCA_IP1{cIdx,s} = squeeze(reshapeByEcc{s}{c,5}(:,:,p,:));
-                RCA_IP2{cIdx,s} = squeeze(reshapeByEcc{s}{c,6}(:,:,p,:));
-                cIdx = cIdx + 1;
-            end
-        end
+% FORMAT: cell array {cnd x subj}(samples x channels x trials)
+
+for a = 1:length(dataSet)
+    for c = 1:nCnd
+        RCA{c,a} = squeeze(data{a}{c});
     end
 end
 
-%% ALEXANDRA'S RCA CODE
-% calculate error bars over # samples/epochs
-subjToInclude = [1:6]; % pick from 'names' vector
-timeCourseLen = 1/(3); %1/3.75; % 3 Hz
-% make more folders 
+switch paradigm    
+    case 'whmSuperSet'
+        % EFFECT OF FREQ: POOLING OVER ECC + POL
+        frq1 = RCA([1:2,7:8,13:14,19:20,25:26,31:32],:);
+        frq2 = RCA([1:2,7:8,13:14,19:20,25:26,31:32]+2,:);
+        frq3 = RCA([1:2,7:8,13:14,19:20,25:26,31:32]+4,:);
+        % EFFECT OF ECCENTRICITY: POOLING OVER TF + POL
+        SF = RCA([1:6],:);
+        SP1 = RCA([1:6]+6,:);
+        SP2 = RCA([1:6]+12,:);
+        IF = RCA([1:6]+18,:);
+        IP1 = RCA([1:6]+24,:);
+        IP2 = RCA([1:6]+30,:);        
+    case 'whmHexCancellation'
+        RCA_UR = RCA([1 5 9 13],:); 
+        RCA_UL = RCA([1 5 9 13]+1,:);
+        RCA_LL = RCA([1 5 9 13]+2,:);
+        RCA_LR = RCA([1 5 9 13]+3,:);
+        RCA_F = RCA([1:4],:);
+        RCA_P1 = RCA([1:4]+1,:);
+        RCA_P2 = RCA([1:4]+2,:);       
+    case 'whmCancellation2Sz'
+        all20 = RCA([1,3,5,7,9,11,13:16],:);
+        all14 = RCA([2,4,6,8,10,12,17:20],:);
+end
+
+%% RUN RCA
+time = 1000/3; % [1000/(30/11),1000/3,1000/3.75] 
+timeCourseLen = repmat(time,[1,7]); 
+cnds = {RCA_UR RCA_UL RCA_LL RCA_LR RCA_F RCA_P1 RCA_P2}; % {allUL allLL allF allP1 allP2} | {frq1 frq2 frq3}
+cndNames = {'UR' 'UL' 'LL' 'LR' 'F' 'P1' 'P2'}; % {'allUL' 'allLL' 'allF' 'allP1' 'allP2'} | {'frq1' 'frq2' 'frq3'}
 tic
-[rcaDataALL, W, A] = rcaRunProject(allUR(1,subjToInclude), RCAfolder, timeCourseLen, 'UR_F'); % RCA_SF(1:2,:) for superset | allP2 for hexCancellation
+for i = 1:length(cnds)
+    [rcaDataALL, W, A] = rcaRunProject(cnds{i}, RCAfolder, timeCourseLen(i), cndNames{i}, nPol);
+%     [rcaDataALL, W, A] = rcaRunProject(RCA(9:10,:), RCAfolder, 1000/3, 'cnd9v10', nPol);
+end
 toc
 
-%% ADAPTING MAX DIFF
-cnd = [1,4];
-for s = 1:length(names)-1
-    data1{s} = squeeze(fullData(:,:,cnd(1),:,s));
-    data1{s} = permute(data1{s},[3 1 2]);
-    data2{s} = squeeze(fullData(:,:,cnd(2),:,s));
-    data2{s} = permute(data2{s},[3 1 2]);
-end
-dataLabels = {'UR-F' 'LR-F'};
-timeCourseLen = 1000/3;
-maxDiff(data1, data2, dataLabels, folder, timeCourseLen)
-
-%% MANUAL RCA
-for a = 1:size(RCA_SF_2,1)
-    for b = 1:size(RCA_SF_2,2)
-        avgRCA_SF_2{a,b} = squeeze(nanmean(RCA_SF_2{a,b},3));
-        for c = 1:size(avgRCA_SF_2{a,b},1)
-            for d = 1:128
-                filtered_SF_2{a,b}(c,d) = avgRCA_SF_2{a,b}(c,d)*W(d,1);
-            end
-        end
-        RC1_SF_2{a,b} = squeeze(nanmean(filtered_SF_2{a,b},2));
-    end
-end
-
-%% whmHexFreq timecourse plots
-if strcmp(paradigm,'whmHexFreq')
-    for s = 1:length(dataSet)
-        frq1(:,:,:,s) = permute(fnlData{s}{1},[1,3,2,4]); % 128 x 6 x 112 x 3
-        frq2(:,:,:,s) = permute(fnlData{s}{2},[1,3,2,4]); % 128 x 6 x 154 x 3
-        frq3(:,:,:,s) = permute(fnlData{s}{3},[1,3,2,4]); % 128 x 2 x 140 x 3
-    end
-else
-    for s = 1:length(dataSet)
-        for c = 1:length(fnlData{s})
-            fnlDataP{s}{c} = permute(fnlData{s}{c},[1 3 2]); % chan x cnd x timept
-        end
-        reshapeByFrq{s} = reshape(fnlDataP{s},length(stimFrq),[])';
-    end
-end
+%% AVERAGED TIMECOURSE PLOTS / INCOMPLETE
 
 chanROI = 71:76;
 cndROI = [1];
 subj = '0011'; % '*AY' | '1368' | '1369' | 'avg'
 yLim = [-600 300];
-plotTimecourse(reshapeByFrq{1}{4,1},subj,yLim,chanROI,cndROI,cndLabels,names,folder)
+plotTimecourse(reshapeByFrq{1}{4,1},subj,yLim,chanROI,cndROI,cndLabels,names,parentDir)
 % [nChan x nCnd x nTimept x nSubj]
-%% PLOTTING EVERY CHANNEL
-cnd = 'UL,F';
+
+%% CODE CHECK: RAW TIMECOURSE PLOTS
+cnd = 'UR,F';
+numTimept = size(avgedData{2}{1},1);
+yLim = [-300 300];
+gcaOpts = {'XTick',linspace(0,numTimept,5),'XTickLabel',{'0','83.33','166.67','250','333.33'},...
+    'XLim',[0 numTimept],'YLim',yLim,'box','off','tickdir','out',...
+    'fontname','Helvetica','linewidth',1.5,'fontsize',10};
+
 for i = 1:128
     subplot(16,8,i)
     hold on
-    plot(squeeze(nanmean(fnlData(i,2,:,:),4)),'color','b')
+    plot([1:numTimept],zeros(1,numTimept),'color','k')
+    plot(squeeze(avgedData{2}{1}(:,i)),'color','b')  
     title(sprintf('%s, Ch. %d',cnd,i))
+    set(gca,gcaOpts{:})
 end
